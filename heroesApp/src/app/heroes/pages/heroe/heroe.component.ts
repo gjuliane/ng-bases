da1a55cc24d064e42af24c048cbf64670f1ab65e
Gustavo Julián <gjuliane@gmail.com>
2021-09-30 23:49:52 -0500

    heroes

--- heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
+++ heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { switchMap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-heroe',
@@ -8,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  idHeroe!: string;
+  heroe!:Heroe;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroeService: HeroesService
+  ) { 
+
+  }
 
   ngOnInit(): void {
+    // this.activatedRoute.params.subscribe(
+    //   params => {
+    //     this.idHeroe = params.id;
+    //     this.heroeService.getHeroePorId(this.idHeroe).subscribe(
+    //       heroe => this.heroe = heroe
+    //     )
+    //   }
+    // )
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroeService.getHeroePorId(id))
+    )
+    .subscribe(
+      heroe => this.heroe = heroe
+    );
   }
 
 }
